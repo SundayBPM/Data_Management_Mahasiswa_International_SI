6f8e388d012ae6801371cfc4c9e26e6ed9a32b74
LAPTOP-94BLRKSF\S U N D A Y <sundaybontong@gmail.com>
2024-11-27 12:54:22 +0700

    update

--- routes.py
+++ routes.py
@@ -121,6 +121,9 @@ def register_routes(app, db):
     
     @app.route('/IISMA', methods=['GET', 'POST'])
     def iisma():
+        """
+        Routes untuk masuk ke halaman form IISMA
+        """
         username = session.get('username')
         if username is None:
             return redirect(url_for('home_page'))
@@ -130,6 +133,7 @@ def register_routes(app, db):
             student_inf = db.session.query(Mahasiswa).filter(Mahasiswa.nim == user_id).first()
             iisma_inf = db.session.query(ExchangeOutbound).filter(ExchangeOutbound.nim == user_id).first()
             return render_template('iisma.html', student_inf=student_inf, iisma_inf=iisma_inf)
+        
         elif request.method == 'POST':
             # mahasiswa = Mahasiswa.query.filter_by(nim = user_id).first()
             exch_info = ExchangeOutbound.query.filter_by(nim = user_id).first()
@@ -188,3 +192,39 @@ def register_routes(app, db):
         exch_info.update_gpa = datetime.strptime(update_gpa, '%Y-%m-%d').date()
         db.session.commit()
         return redirect(url_for('iisma_view', user_id=user_id))
+    
+    @app.route('/student-exchange', methods=['GET', 'POST'])
+    def exchange():
+        """
+        Routes untuk masuk ke halaman form IISMA
+        """
+        username = session.get('username')
+        if username is None:
+            return redirect(url_for('home_page'))
+
+        user_id = session.get('id')
+        if request.method == 'GET':
+            student_inf = db.session.query(Mahasiswa).filter(Mahasiswa.nim == user_id).first()
+            iisma_inf = db.session.query(ExchangeOutbound).filter(ExchangeOutbound.nim == user_id).first()
+            return render_template('iisma.html', student_inf=student_inf, iisma_inf=iisma_inf)
+        
+        elif request.method == 'POST':
+            # mahasiswa = Mahasiswa.query.filter_by(nim = user_id).first()
+            exch_info = ExchangeOutbound.query.filter_by(nim = user_id).first()
+            exch_info.status = request.form['status']
+            exch_info.intake_year = datetime.strptime(request.form['intake_year'], '%Y-%m-%d').date()
+            exch_info.intake = datetime.strptime(request.form['intake'], '%Y-%m-%d').date()
+            from_date = request.form['from']
+            exch_info.from_date = datetime.strptime(from_date, '%Y-%m-%d').date()
+            until = request.form['until']
+            exch_info.until = datetime.strptime(until, '%Y-%m-%d').date()
+            exch_info.sem_at_telu = request.form['semester']
+            # exch_info.semester_telu = request.form['sem_at_telu']
+            # exch_info.gpa = request.form['gpa']
+            exch_info.sem_at_exch = request.form['semester_at_iisma']
+            exch_info.gpa = request.form['gpa_at_iisma']
+            update_gpa = request.form['latest_update_iisma']
+            exch_info.update_gpa = datetime.strptime(update_gpa, '%Y-%m-%d').date()
+            db.session.commit()
+            
+            return redirect(url_for('iisma'))    
